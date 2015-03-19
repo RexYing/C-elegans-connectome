e96a9d2e5dbbcc45457b4e8933dd4fa4d4d32415
Justin O'Hare <jkohare14@aol.com>
2015-03-19 11:45:25 -0400

    two updates

--- code/hub/AdjMat2HubPlot.m
+++ code/hub/AdjMat2HubPlot.m
@@ -1,4 +1,4 @@
-function AdjMat2HubPlot(A)
+function AdjMat2HubPlot(A,varargin)
 %Takes adjacency matrix A as input and generates hub plot
 %
 %things to consider: 
@@ -13,8 +13,15 @@ function AdjMat2HubPlot(A)
 %Justin O'Hare, 2/09/15
 %ohare@neuro.duke.edu
 %%%%%%%%%%%%%%%%%%%%%%%%%
+if ~isempty(varargin)
+    titleText=varargin{1};
+end
+
+A(isnan(A))=0;
+scaleFactor=4;
 hubScores=sum(A,2);
-N=size(A,1);
+sizes=hubScores*scaleFactor+eye;
+N=max(size(A));
 angleInt=2*pi/N;
 xCoords=zeros(N,1);
 yCoords=zeros(N,1);
@@ -30,19 +37,39 @@ end
 %plot the data
 [heatMap,ref]=HeatMapGen(hubScores,max(hubScores));
 figure
-for i=1:N
+for i=1:size(A,1)
+    if size(A,1)<N
+        j=i*(floor(N/length(hubScores)));
+        J(i)=j;
+    else
+        j=i;
+    end
     tempColor=[rand rand rand];
     cnnxnInds=(A(i,:)~=0).*[1:N];
     cnnxnInds(cnnxnInds==0)=[];
     numCnnxns=length(cnnxnInds);%not # of vertices, but # of connected nodes
-    X=[repmat(xCoords(i),1,numCnnxns);xCoords(cnnxnInds)'];
-    Y=[repmat(yCoords(i),1,numCnnxns);yCoords(cnnxnInds)'];
+    X=[repmat(xCoords(j),1,numCnnxns);xCoords(cnnxnInds)'];
+    Y=[repmat(yCoords(j),1,numCnnxns);yCoords(cnnxnInds)'];
     line(X,Y,'Color',tempColor)
     hold on
 end
-scatter(xCoords,yCoords,hubScores*2+eye,heatMap,'filled')
+if size(A,1)<N %if only showing connections from subset of cells ('hubs')
+    sizes2=repmat(min(sizes)/100,N,1);
+    heatMap2=zeros(N,3);
+    for i=1:length(J)
+        sizes2(J(i))=sizes(i);
+        heatMap2(J(i),:)=heatMap(i,:);
+    end
+    sizes=sizes2;clear sizes2
+    heatMap=heatMap2; clear heatMap2
+    
+end    
+scatter(xCoords,yCoords,sizes,heatMap,'filled')
 hold off
 colormap(ref);
-colorbar('YTickLabel',{'0','','',''...
+colorbar('YTickLabel',{sprintf('%0.1f',min(hubScores)),'','',''...
     '','','','','','',sprintf('%0.1f',max(hubScores))});
-title('c. Elegans Connectome Hub Plot')
+if ~exist('titleText','var')
+    titleText='HubPlot';
+end
+title(titleText)
--- code/hub/rankedInds.m
+++ code/hub/rankedInds.m
@@ -1,8 +1,18 @@
-function inds=rankedInds(data)
+function [inds,sortedData]=rankedInds(varargin)
 %return list of indices ranking each entry in ascending order
 %so if data(3) is 144th-highest value in data, inds(3)==144
+%
+%direction is either 'ascend' or 'descend'. 'ascend' is default.
+data=varargin{1};
+if length(varargin)==1
+    direction='ascend';
+else
+    direction=varargin{2};
+end
+
 inds=nan(length(data),1);
-[~,I]=sort(data,1,'ascend');
+
+[sortedData,I]=sort(data,1,direction);
 for i=1:length(I)
     inds(i)=sum((I==i).*[1:length(I)]');
 end
